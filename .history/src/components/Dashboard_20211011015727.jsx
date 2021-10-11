bce53a3d3f48ecfa95e79b393bdf3a8980a0656f
Shalev Hadar <76647060+ShalevHadar@users.noreply.github.com>
2021-10-11 12:00:23 +0300

    Delete .history directory

--- .history/src/components/Dashboard_20211011015727.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Dashboard() {
-    return (
-        <div>
-            <h1>DashBoard</h1>
-        </div>
-    )
-}
--- .history/src/components/Nav_20211011032151.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Nav() {
-    return (
-        <div>
-            Yo Yo Yo
-        </div>
-    )
-}
