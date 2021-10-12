8528bbfaaa7d332abfd45a2f47e3fd5f573775ec
Shalev Hadar <shdar94@gmail.com>
2021-10-12 14:28:06 +0300

    added an items array along with map function

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,21 +1,28 @@
-import {  getAuth, signOut } from "@firebase/auth";
-import { setDoc,  doc, collection } from "@firebase/firestore";
-import React, {  useEffect, useState } from "react";
+import { getAuth, signOut } from "@firebase/auth";
+import {
+  setDoc,
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Form, Stack } from "react-bootstrap";
 import { useHistory } from "react-router";
 import { getDB } from "../utils/firebase";
 
 export default function Dashboard() {
-
-    
-
-    const auth = getAuth();
+  const auth = getAuth();
   const user = auth.currentUser;
-
   const history = useHistory();
-
   const [content, setContent] = useState("");
   const [price, setPrice] = useState(0);
+  const [items, setItems] = useState([]);
+
+  const showItems = () => {
+    console.log(items);
+  };
 
   const onLogout = () => {
     signOut(auth)
@@ -26,18 +33,39 @@ export default function Dashboard() {
       .catch((e) => alert(e.message));
   };
 
-  const  addItem = () => {
+  const addItem = () => {
     const db = getDB();
     const newTaskRef = doc(collection(db, "spends"));
-    console.log(user.uid);
     setDoc(newTaskRef, {
-        text: content,
-        amount: price,
-        user: user.uid
+      text: content,
+      amount: price,
+      user: user.uid,
     });
-  }
+  };
 
-  
+  const fetchItems = async () => {
+    const db = getDB();
+    if (user && user.uid) {
+      const q = query(collection(db, "spends"), where("user", "==", user.uid));
+      try {
+        const results = await getDocs(q);
+        let resultItems = [];
+        results.forEach((doc) => {
+          resultItems.push({
+            ...doc.data(),
+            uid: doc.id,
+          });
+          setItems(resultItems);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  });
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -46,8 +74,6 @@ export default function Dashboard() {
     }
   });
 
-  
-
   return (
     <div>
       <Container className="text-center">
@@ -55,20 +81,23 @@ export default function Dashboard() {
 
         <Stack direction="horizontal" gap={3} className="gap2">
           <Form.Control
-          defaultValue={content}
-          onChange={e => setContent(e.target.value)}
+            defaultValue={content}
+            onChange={(e) => setContent(e.target.value)}
             className="me-auto"
             placeholder="Add your item here..."
           />
           <Form.Control
-          defaultValue={price}
-          onChange={e => setPrice(e.target.value)}
+            type="number"
+            defaultValue={price}
+            onChange={(e) => setPrice(e.target.value)}
             className="me-auto"
             placeholder="Price..."
             style={{ width: "20%" }}
           />
-          <div className="vr" />
-          <Button onClick={addItem}variant="success">Submit</Button>
+          <div></div>
+          <Button onClick={addItem} variant="success">
+            Submit
+          </Button>
         </Stack>
 
         <Button
@@ -80,7 +109,18 @@ export default function Dashboard() {
         >
           Logout
         </Button>
+        <Button
+          variant="danger"
+          className="col-md-3 mx-auto"
+          type="submit"
+          onClick={showItems}
+        >
+          showlogs
+        </Button>
       </Container>
-    </div>
+      {items.map(item => (
+        <div key={item.uid}>{item.text}</div>
+      ))}    
+      </div>
   );
 }
